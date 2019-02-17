0a9ff3f1b494839a50d65b3ae36ee8dab393793d
Margarita Krutikova <margaretkru@gmail.com>
2019-02-17 01:50:23 +0100

    Replace absolute imports with relative

--- src/domains/words/index.ts
+++ src/domains/words/index.ts
@@ -1,2 +1,7 @@
-export { WordEntity, PaginatedWords, createWord, copyWord } from './model';
-export { default as WordsApi } from './service';
+import { PaginatedWords, WordEntity } from "./model"
+
+export { createWord, copyWord } from "./model"
+export { default as WordsApi } from "./service"
+
+export interface WordEntity extends WordEntity {}
+export interface PaginatedWords extends PaginatedWords {}
